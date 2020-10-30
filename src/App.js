2a64138b085c72165b3d31f92091b537f3db3602
Ian Clarke <ian.clarke@fullscript.com>
2020-10-30 13:17:48 -0400

    Init commit
    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import Unsplash from 'unsplash-js';
+
+import logo from './logo.svg';
+import './App.css';
+
+// TODO: Replace "APP_ACCESS_KEY" with your own key, which
+// can be generated here: https://unsplash.com/developers
+const unsplash = new Unsplash({ accessKey: APP_ACCESS_KEY });
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
